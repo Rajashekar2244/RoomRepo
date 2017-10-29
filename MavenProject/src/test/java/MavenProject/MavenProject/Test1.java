9d6973ed2727c4e218b7b7be4e212c2695dbb7d8
raj <rajashekarreddy2244@gmail.com>
2017-10-29 12:28:20 +0530

    Hi

---
+++ MavenProject/src/test/java/MavenProject/MavenProject/Test1.java
@@ -0,0 +1,23 @@
+package MavenProject.MavenProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	WebDriver driver;
+	@BeforeMethod
+	void openBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Raja\\WebDrivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+	}
+@Test
+void firstMethod()
+{
+	System.out.println("first method executed");
+}
+}
