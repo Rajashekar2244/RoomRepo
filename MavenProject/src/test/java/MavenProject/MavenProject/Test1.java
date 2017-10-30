8204ed0e59560d2511f419e74f49de749f199dec
Rajashekar2244 <33120947+Rajashekar2244@users.noreply.github.com>
2017-10-30 22:31:38 +0530

    Update Test1.java

--- MavenProject/src/test/java/MavenProject/MavenProject/Test1.java
+++ MavenProject/src/test/java/MavenProject/MavenProject/Test1.java
@@ -14,6 +14,7 @@ public class Test1 {
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
+		System.out.println("widows opened Successfully");
 	}
 @Test
 void firstMethod()
